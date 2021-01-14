c3b1538ae5ed6cbaa2c227f059d0f5ec2d34bd11
Sebastien <perigon@hotmail.com>
2021-01-14 19:08:10 +0000

    refactoring and mocha test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,28 @@
+
 const request = require("request");
-const argv = process.argv[2];
 
-
-let obj;
-let race = argv;
-let id;
-request(`https://api.thecatapi.com/v1/breeds/search/?q=${race} `, (error, response, body) => {
-  response = body;
-  if (error) {
-    return console.log(error);
-  }
-  obj = JSON.parse(response);
-  id = obj[0];
-  if (id === undefined) {
-    console.log("Breed not found try again !!!");
-  } else {
-    console.log(id.description);
-  }
- 
-});
+const fetchBreedDescription = function(breedName, callback) {
 
 
 
+  
+  request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName} `, (error, response, body) => {
+    response = body;
+    if (error) {
+      return error;
+    }
+    let obj = JSON.parse(response);
+    let id = obj[0];
+    if (id === undefined) {
+      // execute the callback
+      callback("Breed not found", null);
+      // return callback = "Breed not found try again !!!";
+    } else {
+      callback(null, id.description);
+    }
+   
+  });
+};
 
-//[{"breeds":[],"id":"ee6","url":"https://cdn2.thecatapi.com/images/ee6.jpg","width":460,"height":276}]
+module.exports = { fetchBreedDescription };
+
